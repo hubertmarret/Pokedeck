553825af49fe619685c7dce7b6696918f52268bd
hubert <hubert.marret@gmail.com>
2014-11-11 18:37:26 +0100

    add a GUI with the home menu and the add page

--- src/ui/GraphicUserInterface.java
+++ src/ui/GraphicUserInterface.java
@@ -1,24 +1,36 @@
 package ui;
 
-import java.awt.CardLayout;
-
+import java.awt.*;
 import javax.swing.*;
 
 public class GraphicUserInterface extends JFrame {
 
 	private static final long serialVersionUID = 25L;
-	
-	private CardLayout cLayout = new CardLayout();
+		
+	private CardLayout cardLayout = new CardLayout();
 	private JPanel content = new JPanel();
-	private JButton button = new JButton("my button");
+	
+	private PanelConstructor panConstructor = new PanelConstructor();
+	private JPanel menu;
+	private JPanel addPage;
+	
 	
 	public GraphicUserInterface()
 	{
 		this.setTitle("Pokedeck");
-	    this.setSize(300, 150);
+	    this.setSize(900, 450);
 	    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    this.setLocationRelativeTo(null);
-	    content.add(button);
+	    
+	    menu = panConstructor.createMenu(cardLayout, content);
+	    addPage = panConstructor.createAddPage(cardLayout, content);
+	    
+	    content.setBackground(new Color(49, 78, 183));
+	    content.setPreferredSize(new Dimension(800, 430));
+	    content.setLayout(cardLayout);
+	    content.add(menu, "menu");
+	    content.add(addPage, "addPage");
+	    
 	    this.setContentPane(content);
 	    this.setVisible(true);
 	}
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -3,6 +3,7 @@ package ui;
 public class Main {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("unused")
 		GraphicUserInterface gui = new GraphicUserInterface();
 	}
 
